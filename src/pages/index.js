07e47a705c7cf6fcbaf677b714b4af6e55b83c9e
ryo86nfd <ryo86nfd@gmail.com>
2023-01-13 16:00:28 +0900

    [update] 不要な記述の削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,13 @@ class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    // const [author] = get(this, 'props.data.allContentfulPerson.edges')
 
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
-          <Hero data={author.node} />
+          {/* <Hero data={author.node} /> */}
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
             <ul className="article-list">
@@ -64,28 +64,29 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      edges {
-        node {
-          name
-          shortBio {
-            shortBio
-          }
-          title
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-        }
-      }
-    }
   }
 `
+
+    // allContentfulPerson(
+    //   filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
+    // ) {
+    //   edges {
+    //     node {
+    //       name
+    //       shortBio {
+    //         shortBio
+    //       }
+    //       title
+    //       heroImage: image {
+    //         fluid(
+    //           maxWidth: 1180
+    //           maxHeight: 480
+    //           resizingBehavior: PAD
+    //           background: "rgb:000000"
+    //         ) {
+    //           ...GatsbyContentfulFluid_tracedSVG
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
